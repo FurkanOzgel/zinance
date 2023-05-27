f733e2ce687e989e313a90090bb63ff5dff12a98
Furkan Özgel <furkan.ozgel@hotmail.com>
2023-05-27 14:55:52 +0300

    Added Cryptocurrency Detail Page

--- js/marketSummary.js
+++ js/marketSummary.js
@@ -1,16 +1,15 @@
 import data from '../data/coinList.json' assert { type: 'json' };
 
-
-
-
 data.forEach((item)=>{
     let list= document.getElementsByClassName("all-coins")[0]
     let div = document.createElement("div")
-    if(item.percentage > 0){
+    if(item.percentage >= 0){
         div.innerHTML = `
         <div  class = "coin-list-item" onclick = "goDetailPage(this)">
-            <span class="item-symbol">`+item.symbol.slice(0,-3)+`</span>
-            <span class="item-price">₺`+ Math.round(item.price*100)/100+`</span>
+            <span class="item-symbol">
+                <img class="icon" src="../../images/crypto_icons/`+item.symbol.slice(0,-3).toLocaleLowerCase()+`.png">
+                `+item.symbol.slice(0,-3)+`</span>
+            <span class="item-price">₺`+ Number(item.price)+`</span>
             <div class="item-change increase"><div id="item-change-div">`+Math.round(item.percentage*100)/100+`%</div></div>
             <span class="item-volume">₺`+Math.round(item.volume*100)/100+`</span>
         </div>
@@ -18,8 +17,10 @@ data.forEach((item)=>{
     }else{
         div.innerHTML = `
         <div  class = "coin-list-item" onclick = "goDetailPage(this)">
-            <span class="item-symbol">`+item.symbol.slice(0,-3)+`</span>
-            <span class="item-price">₺`+ Math.round(item.price*100)/100+`</span>
+            <span class="item-symbol">
+                <img class="icon" src="../../images/crypto_icons/`+item.symbol.slice(0,-3).toLocaleLowerCase()+`.png">
+                `+item.symbol.slice(0,-3)+`</span>
+            <span class="item-price">₺`+ Number(item.price)+`</span>
             <div class="item-change down"><div id="item-change-div">`+Math.round(item.percentage*100)/100+`%</div></div>
             <span class="item-volume">₺`+Math.round(item.volume*100)/100+`</span>
         </div>
@@ -41,18 +42,22 @@ most_increased.forEach((item) => {
 
     let list = document.getElementById("raise-list")
     let div = document.createElement("div")
-    if(item.percentage > 0){
+    if(item.percentage >= 0){
         div.innerHTML = `
-        <div style="display: flex; justify-content: space-between;">
-            <span class="symbol">`+item.symbol.slice(0,-3)+`</span>
+        <div style="display: flex; justify-content: space-between;  margin-bottom: 7px;">
+            <span class="symbol">
+                <img style="width: 24px; height: 24px;" class="icon" src="../../images/crypto_icons/`+item.symbol.slice(0,-3).toLocaleLowerCase()+`.png">
+                `+item.symbol.slice(0,-3)+`</span>
             <span class="price">₺`+ Math.round(item.price*100)/100+`</span>
             <span class="change increase">`+Math.round(item.percentage*100)/100+`%</span>
         </div>
         `
     }else{
         div.innerHTML = `
-        <div style="display: flex; justify-content: space-between;">
-            <span class="symbol">`+item.symbol.slice(0,-3)+`</span>
+        <div style="display: flex; justify-content: space-between; margin-bottom: 7px;">
+            <span class="symbol">
+                <img style="width: 24px; height: 24px;" class="icon" src="../../images/crypto_icons/`+item.symbol.slice(0,-3).toLocaleLowerCase()+`.png">
+                `+item.symbol.slice(0,-3)+`</span>
             <span class="price">₺`+ Math.round(item.price*100)/100+`</span>
             <span class="change down">`+Math.round(item.percentage*100)/100+`%</span>
         </div>
@@ -81,18 +86,22 @@ most_volume.forEach((item) => {
         price = Math.round(item.price*100)/100
     }
 
-    if(item.percentage > 0){
+    if(item.percentage >= 0){
         div.innerHTML = `
-        <div style="display: flex; justify-content: space-between;">
-            <span class="symbol">`+item.symbol.slice(0,-3)+`</span>
+        <div style="display: flex; justify-content: space-between; margin-bottom:7px;">
+            <span class="symbol">
+                <img style="width: 24px; height: 24px;" class="icon" src="../../images/crypto_icons/`+item.symbol.slice(0,-3).toLocaleLowerCase()+`.png">
+                `+item.symbol.slice(0,-3)+`</span>
             <span class="price">₺`+ price+`</span>
             <span class="change increase">`+Math.round(item.percentage*100)/100+`%</span>
         </div>
         `
     }else{
         div.innerHTML = `
-        <div style="display: flex; justify-content: space-between;">
-            <span class="symbol">`+item.symbol.slice(0,-3)+`</span>
+        <div style="display: flex; justify-content: space-between; margin-bottom:7px;">
+            <span class="symbol">
+                <img style="width: 24px; height: 24px;" class="icon" src="../../images/crypto_icons/`+item.symbol.slice(0,-3).toLocaleLowerCase()+`.png">
+                `+item.symbol.slice(0,-3)+`</span>
             <span class="price">₺`+ price +`</span>
             <span class="change down">`+Math.round(item.percentage*100)/100+`%</span>
         </div>
