adfd6f8758bc1546e8f90696b140834f6537abf0
Furkan Özgel <furkan.ozgel@hotmail.com>
2023-06-12 09:38:45 +0300

    Changed Default Chart Colors

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,32 +1,30 @@
 const budget = document.getElementById('graph1');
-
+Chart.defaults.borderColor = "#5a5a5a"
+Chart.defaults.color = 'white';
 
 new Chart(budget, {
     type: 'doughnut',
     data: {
         labels: ["TRX", "ALPINE", "EOS", "AVAX"],
         datasets: [{
-            label: ' of Votes',
+            label: ' Value in Budget',
             data: [12, 19, 3, 5],
             borderWidth: 0
         }]
     },
     options: {
         scales: {
+            x: {
+              ticks: {
+                color: "white"
+              }
+            },
             y: {
-                beginAtZero: true
+              ticks: {
+                color: "white"
+              }
             }
-        },
-        plugins: {
-            legend: {
-                labels: {
-                    // This more specific font property overrides the global property
-                    font: {
-                        style: "color:white"
-                    }
-                }
-            }
-        }
+          }
     }
 });
 
@@ -37,12 +35,20 @@ const self_improvement = document.getElementById('graph2');
 new Chart(self_improvement, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 0
-        }]
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+            {
+                label: "Value of Budget",
+                fillColor : "rgba(255, 89, 114, 0.6)",
+                strokeColor : "rgba(51, 51, 51, 1)",
+                pointColor : "rgba(255, 89, 114, 1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(151,187,205,1)",
+                maintainAspectRatio: false,
+                data : [10000, 6800, 5500, 8000, 7700, 9200, 12300]
+            }
+        ]
     },
     options: {
         scales: {
