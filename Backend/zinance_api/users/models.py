31574b666e249ba7748369aada621bcfd3571b2a
Furkan Özgel <furkan.ozgel@hotmail.com>
2023-06-27 19:15:27 +0300

    User Model Updated

--- Backend/zinance_api/users/models.py
+++ Backend/zinance_api/users/models.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField(default="2006-09-30")
+    
     username = None
 
     USERNAME_FIELD = 'email'
--- Backend/zinance_api/users/serializers.py
+++ Backend/zinance_api/users/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email", "password"]
+        fields = ["id", "first_name", "last_name", "birth_date", "email", "password"]
         extra_kwargs = {
             'password': {'write_only': True}
         }
