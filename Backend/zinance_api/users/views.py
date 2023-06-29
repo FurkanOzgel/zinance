4874e5ac98c7e11c2c7fad40adf61de23cffb912
Furkan Özgel <furkan.ozgel@hotmail.com>
2023-06-29 23:12:23 +0300

    Updated jwt token expiration time

--- Backend/zinance_api/users/views.py
+++ Backend/zinance_api/users/views.py
@@ -33,7 +33,7 @@ class LoginView(APIView):
 
         payload = {
             "id": user.id,
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=60),
             "iat": datetime.datetime.utcnow()
         }
 
@@ -54,6 +54,7 @@ class UserView(APIView):
         if not token:
             raise exceptions.AuthenticationFailed("Unauthenticated!")
 
+        payload = jwt.decode(token, "secret", algorithms=["HS256"])
 
         try:
             payload = jwt.decode(token, "secret", algorithms=["HS256"])
