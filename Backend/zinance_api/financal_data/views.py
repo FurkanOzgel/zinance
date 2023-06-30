f8f95594245c5be2081cc3667d9b6dcd8a9a6afb
Furkan Özgel <furkan.ozgel@hotmail.com>
2023-06-30 15:54:41 +0300

    Response type updated as json object

--- Backend/zinance_api/financal_data/views.py
+++ Backend/zinance_api/financal_data/views.py
@@ -4,6 +4,8 @@ from rest_framework import exceptions
 import subprocess
 import os.path
 import json
+import re
+import re
 
 
 class PriceOfView(APIView):
@@ -12,7 +14,16 @@ class PriceOfView(APIView):
         script_path = os.path.dirname(os.path.abspath(__file__)) + "/node_script/getPrice.js"
         price_data = subprocess.run(["node", script_path, instrument], capture_output=True, text=True)
 
-        price_data = price_data.stdout.replace("\n", "")
+        price_data = price_data.stdout.replace("\n", "").replace("'",'"')
+        # İki nokta karakteri kullanılan yerleri tespit etmek için regex deseni
+        pattern = r"\b(\w+)\s*:\s*"
+
+        # İki nokta karakteri kullanılan yerleri bulma ve düzenleme
+        price_data = re.sub(pattern, r'"\1":', price_data)
+
+        
+
+        price_data =json.loads(price_data)
         
         return Response(price_data)
 
